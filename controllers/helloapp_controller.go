bbf502918475449a28dd9b5ab3eff793e37c7287
Dmitry Zakharov <zakharov@ie.ibm.com>
2023-02-22 18:19:59 +0000

    add labels

--- controllers/helloapp_controller.go
+++ controllers/helloapp_controller.go
@@ -90,6 +90,9 @@ func (this *HelloAppReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 			deployment.Spec = apps.DeploymentSpec{
 				Replicas: &size,
 				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{"a": "b"},
+					},
 					Spec: corev1.PodSpec{
 						Containers: []corev1.Container{{
 							Image: image,
