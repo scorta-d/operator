840c1d5054cd84d040614770d7f2fef85f6a5cc8
Dmitry Zakharov <zakharov@ie.ibm.com>
2023-02-22 10:26:08 +0000

    log message

--- controllers/helloapp_controller.go
+++ controllers/helloapp_controller.go
@@ -47,7 +47,7 @@ type HelloAppReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *HelloAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	var log = log.FromContext(ctx)
 
 	// your logic here
 	log.Info("Process")
