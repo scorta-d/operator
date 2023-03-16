a50b2fa7ac3580cffa7308738f51159f3861dded
Dmitry Zakharov <zakharov@ie.ibm.com>
2023-03-15 18:02:27 -0700

    set ns in resize sc ript

--- 06-resize-3.sh
+++ 06-resize-3.sh
@@ -5,7 +5,7 @@ yq -i -e ".spec.size=3" apps_v1_helloapp.yaml
 
 echo new size: $(yq '.spec.size'  apps_v1_helloapp.yaml)
 
-oc apply -f  apps_v1_helloapp.yaml
+oc apply -f  apps_v1_helloapp.yaml -n test 
 
 cd ../..
 watch ./05-verify.sh
